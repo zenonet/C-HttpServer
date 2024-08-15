15a363314fca75a6ab769d23e26570c697fac83e
zenonet <78077158+zenonet@users.noreply.github.com>
2024-08-15 20:04:45 +0200

    Added structs for request and responses etc.

--- main.c
+++ main.c
@@ -1,17 +1,58 @@
  
 // #include <iostream> 
+// #include <cstdio>
 #include <netinet/in.h> 
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h> 
 #include <unistd.h>
 #include <stdlib.h>
-/*
-char* response(int code, char* body){
-    char statusLine[] = "HTTP/1.0 XXX OK";
-    return &statusLine[0];
-}
-*/
 
+enum HttpMethod : char{
+    GET,
+    POST,
+    PUT,
+    DELETE
+};
+
+
+struct HttpRequest{
+    enum HttpMethod method;
+    char* uri;
+    int clientSocket;
+    char isClosed;
+};
+
+struct HttpResponse{
+    int statusCode;
+    char* body;
+};
+
+typedef struct HttpResponse* (*httpRequestHandler)(struct HttpRequest*);
+
+char* getResponse(int code, char* body){
+   if(code < 0 || code > 999) return NULL;
+   int bodyLength = strlen(body)-1;
+   char statusLine[] = "HTTP/1.0 XXX OK\n\n";
+   int statusLineLength = sizeof(statusLine)-1;
+   sprintf(&statusLine[9], "%d", code);
+   statusLine[12] = ' '; // Because sprintf adds a null character after the number, we need to make that a space again
+   char* response = malloc(statusLineLength+bodyLength+1);
+
+   memcpy(response, statusLine, statusLineLength);
+   memcpy(&response[statusLineLength], body, bodyLength);
+   response[statusLineLength+bodyLength] = 0;
+   printf("Response length: %lu\n", strlen(response));
+   return response;
+}
+
+void sendResponse(struct HttpRequest* request, struct HttpResponse* response){
+    char* responseString = getResponse(response->statusCode, response->body);
+    send(request->clientSocket, responseString, strlen(responseString), 0);
+    free(responseString);
+    close(request->clientSocket);
+    request->isClosed = 1;
+}
 
 int main(){
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -22,7 +63,7 @@ int main(){
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
     int success = bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-
+    printf("Return value of bind call is %d\n", success);
     listen(serverSocket, 5);
     printf("Now listening...\n");
     char quit = 0;
@@ -39,22 +80,33 @@ int main(){
         char* method = (char*) malloc(methodLength+1);
         memcpy(method, &buffer, methodLength);
         method[methodLength] = 0;
-        // std::cout << "HTTP Method: " << method << '\n';
 
         int uriLength = 0;
         while(uriLength + methodLength + 1 < sizeof(buffer) && buffer[uriLength+methodLength+1] != ' ') uriLength++; 
         char* uri = (char*) malloc(uriLength);
         memcpy(uri, &buffer[methodLength+1], uriLength+1);
         uri[uriLength] = 0;
-        printf("Path: %s\n", uri);
-        quit = !strcmp(uri, "/close"); 
 
-        char response[] = "HTTP/1.0 200 OK\n\nHello World!";
+        struct HttpRequest* request = (struct HttpRequest*)malloc(sizeof(struct HttpRequest));
+        request->isClosed = 0;
+        request->clientSocket = clientSocket;
+        request->uri = uri;
 
-        
+        // translate method string to enum value
+        if(strcmp(method, "GET")) request->method = GET;
+        if(strcmp(method, "POST")) request->method = POST;
+        if(strcmp(method, "PUT")) request->method = PUT;
+        if(strcmp(method, "DELETE")) request->method = DELETE;
+       
 
+    
+        quit = !strcmp(request->uri, "/close"); 
+
+        //char response[] = "HTTP/1.0 200 OK\n\nHello World!";
+        char* response = getResponse(200, "Dazu noch ein bisschen frisches Asbest!");
+        printf("Sending: '%s'\n", response);
         send(clientSocket, response, strlen(response), 0);
-
+        free(response);
         close(clientSocket);
     }
 }
