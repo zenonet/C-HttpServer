7fc6ba5f6d9f876857d84761e5245e31475ec453
zenonet <78077158+zenonet@users.noreply.github.com>
2024-08-15 21:32:14 +0200

    Added a system for function pointer based request handling

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
  
-// #include <iostream> 
-// #include <cstdio>
 #include <netinet/in.h> 
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h> 
 #include <unistd.h>
 #include <stdlib.h>
+
+#define PORT 8080
 
 enum HttpMethod : char{
     GET,
@@ -16,30 +16,35 @@ enum HttpMethod : char{
 };
 
 
-struct HttpRequest{
+typedef struct HttpRequest{
     enum HttpMethod method;
     char* uri;
     int clientSocket;
     char isClosed;
-};
+} HttpRequest;
 
-struct HttpResponse{
+typedef struct HttpResponse{
     int statusCode;
     char* body;
-};
+} HttpResponse;
 
-typedef struct HttpResponse* (*httpRequestHandler)(struct HttpRequest*);
+typedef HttpResponse* (*httpRequestHandler)(HttpRequest*);
+
+int serverSocket;
 
 char* getResponse(int code, char* body){
    if(code < 0 || code > 999) return NULL;
-   int bodyLength = strlen(body)-1;
-   char statusLine[] = "HTTP/1.0 XXX OK\n\n";
+   int bodyLength = strlen(body);
+   char statusLine[] = "HTTP/1.0 XXX OK\nContent-Type: text/html\n\n";
    int statusLineLength = sizeof(statusLine)-1;
    sprintf(&statusLine[9], "%d", code);
    statusLine[12] = ' '; // Because sprintf adds a null character after the number, we need to make that a space again
    char* response = malloc(statusLineLength+bodyLength+1);
 
+   // Copy the status line to the response string
    memcpy(response, statusLine, statusLineLength);
+
+   // Copy the response body to the response string
    memcpy(&response[statusLineLength], body, bodyLength);
    response[statusLineLength+bodyLength] = 0;
    printf("Response length: %lu\n", strlen(response));
@@ -49,21 +54,45 @@ char* getResponse(int code, char* body){
 void sendResponse(struct HttpRequest* request, struct HttpResponse* response){
     char* responseString = getResponse(response->statusCode, response->body);
     send(request->clientSocket, responseString, strlen(responseString), 0);
+    printf("Sent response\n");
     free(responseString);
-    close(request->clientSocket);
-    request->isClosed = 1;
 }
 
+
+HttpResponse* myHandler(HttpRequest* request){
+
+    if(!strcmp(request->uri, "/close")){
+        printf("Terminating because /close endpoint was called..\n");
+        close(serverSocket);
+        exit(0);
+    }
+    printf("Receved request for '%s'\n", request->uri);
+    HttpResponse* response = malloc(sizeof(HttpResponse));
+    response->body = "Hello Hackclub! <br><a href=\"/close\">Close server</a>";
+    response->statusCode = 200;
+    return response;
+}
+
+httpRequestHandler globalHandler;
 int main(){
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    globalHandler = &myHandler;  
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_port = htons(PORT);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    int success = bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    printf("Return value of bind call is %d\n", success);
+    int problem = bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    printf("Return value of bind call is %d\n", problem);
+
+    if(problem){
+        printf("Unable to bind socket for port %d. Terminating...\n", PORT);
+        return 1;
+    }
+
     listen(serverSocket, 5);
     printf("Now listening...\n");
     char quit = 0;
@@ -99,14 +128,20 @@ int main(){
         if(strcmp(method, "DELETE")) request->method = DELETE;
        
 
-    
-        quit = !strcmp(request->uri, "/close"); 
+        HttpResponse* response = globalHandler(request);
+
+        sendResponse(request, response);
+        close(request->clientSocket);
+        request->isClosed = 1;
+
+        // quit = !strcmp(request->uri, "/close"); 
 
         //char response[] = "HTTP/1.0 200 OK\n\nHello World!";
-        char* response = getResponse(200, "Dazu noch ein bisschen frisches Asbest!");
-        printf("Sending: '%s'\n", response);
-        send(clientSocket, response, strlen(response), 0);
+        //char* response = getResponse(200, "Dazu noch ein bisschen frisches Asbest!");
+        //printf("Sending: '%s'\n", response);
+        //send(clientSocket, response, strlen(response), 0);
+        // free(response->body);
         free(response);
-        close(clientSocket);
+        // close(clientSocket);
     }
 }
